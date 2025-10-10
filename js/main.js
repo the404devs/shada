2db75f03e8f4da07a61ebe9803c4f8ec8d5a24f7
Owen Bowden (the404) <47672957+the404devs@users.noreply.github.com>
2025-10-09 20:38:07 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -233,6 +233,7 @@ function loadMonthFromLocalStorage(key) {
 }
 
 function changeMonth(delta) {
+	saveMonthToLocalStorage();
 	// if (confirm("Changing the month will clear the current calendar. Do you want to continue?")) {
 		month += delta;
 		if (month < 1) {
