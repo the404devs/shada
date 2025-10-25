1d8fdbb4ac7dab1c7f8afc45df68a322c0f18266
Owen Bowden (the404) <47672957+the404devs@users.noreply.github.com>
2025-10-25 15:15:48 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -108,7 +108,7 @@ function generateTable(year, month) {
 		num.textContent = date.getUTCDate();
 		elem.appendChild(num);
 
-		let index = date.getUTCDay() + 2;
+		let index = date.getUTCDay() + 1;
 		if (index > 7) { index = 1 }
 
 		elem.style.gridColumnStart = index;
